6e4c6770032a9631f5abaa5e0a9f89eea6b5a84b
KaanOzdemir <kaan.ozdemir@tsoft.com.tr>
2021-12-07 14:00:49 +0300

    Initial Commit

---
+++ GifExample/GifExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GifExampleApp.swift
+//  GifExample
+//
+//  Created by Kaan Ozdemir on 7.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GifExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
